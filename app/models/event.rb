99b84afd2914470dac55c172768a21d106af452d
Justin Pincar <justinpincar@gmail.com>
2011-10-02 11:23:25 -0700

    Mostly styling.

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,8 @@ class Event
     event._id = event_params["_id"] if event_params["_id"].present?
     event.type = event_params["type"].to_i if event_params["type"].present?
     event.display = event_params["display"] if event_params["display"].present?
+    event.c_at = event_params["c_at"] if event_params["c_at"].present?
+    event.u_at = event_params["u_at"] if event_params["u_at"].present?
     event
   end
 
@@ -22,7 +24,7 @@ class Event
       self._id = BSON::ObjectId.new
       @@db.collection("events_#{account_id}").insert({_id: _id, display: display, type: type, c_at: now, u_at: now})
     else
-      @@db.collection("events_#{account_id}").update({_id: _id}, {display: display, type: type, u_at: now})
+      @@db.collection("events_#{account_id}").update({_id: _id},  {"$set" => {display: display, type: type, u_at: now}})
     end
   end
 
@@ -38,10 +40,10 @@ class Event
     events
   end
 
+  @events = {}
   def self.find(account_id, id)
     id = BSON::ObjectId.from_string(id) if id.is_a?(String)
-    params = @@db.collection("events_#{account_id}").find({_id: id}).first
-    generate(params)
+    @events[id] ||= generate(@@db.collection("events_#{account_id}").find({_id: id}).first)
   end
 
   private
