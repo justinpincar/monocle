337d148cc0d72092be4b07df7845f1accb9e76a3
Justin Pincar <justinpincar@gmail.com>
2012-04-07 21:25:01 -0700

    Modifying monocle javascript to use socket.io

--- public/javascripts/monocle.js
+++ public/javascripts/monocle.js
@@ -6,7 +6,7 @@
 
         if (typeof local_messages != "undefined") {
             for (var i = 0; i < local_messages.length; i++) {
-                window.monocle.receive({data: local_messages[i]});
+                window.monocle.receive(local_messages[i]);
             }
         }
 
@@ -17,31 +17,30 @@
         var _self = this;
 
         this.join = function() {
-            var cometdURL = location.protocol + "//" + location.host + config.contextPath + "/cometd";
-
-            $.cometd.websocketEnabled = true;
-            $.cometd.configure({
-                url: cometdURL,
-                logLevel: 'debug'
-            });
-            $.cometd.handshake();
+	    var monocle_socket = io.connect('http://trisse.com:8000/monocle_meta');
+	    monocle_socket.on('connect', function () {
+		    $('#monocle-status').html("Connected");
+		    $('#monocle-status').css("border", "1px solid black");
+		    $('#monocle-status').css("color", "black");
+		});
+	    monocle_socket.on('connect_failed', function () {
+		    $('#monocle-status').html("Disconnected");
+		    $('#monocle-status').css("border", "1px solid red");
+		    $('#monocle-status').css("color", "red");
+		});
+	    monocle_socket.on('analytic', this.receive);
         };
 
-        this.leave = function() {
-            _unsubscribe();
-            $.cometd.disconnect();
-        };
+        this.receive = function(data) {
+            window.messages.push(data);
 
-        this.receive = function(message) {
-            window.messages.push(message);
-
-            var sessionId = message.data.s;
-            var eventId = message.data.d.e;
+            var sessionId = data.s;
+            var eventId = data.d.e;
             if (typeof eventId != "undefined" && eventId) {
                 var event = events[eventId];
             }
-            var href = message.data.d.h;
-            var ts = message.data.ts;
+            var href = data.d.h;
+            var ts = data.ts;
 
             var block = $('#' + sessionId);
             if (block.length == 0) {
@@ -95,77 +94,5 @@
             }
             window.timeouts[sessionId] = setTimeout(function() {block.hide('slow')}, 1000 * 60 * 10);
         };
-
-        function _unsubscribe() {
-            if (_chatSubscription) {
-                $.cometd.unsubscribe(_chatSubscription);
-            }
-            _chatSubscription = null;
-        }
-
-        function _subscribe() {
-            _chatSubscription = $.cometd.subscribe('/chat/' + channel, _self.receive);
-        }
-
-        function _connectionInitialized() {
-            _subscribe();
-        }
-
-        function _connectionEstablished() {
-            alert("Connection established.");
-        }
-
-        function _connectionBroken() {
-            alert("Connection broken.");
-        }
-
-        function _connectionClosed() {
-            alert("Connection closed.");
-        }
-
-        function _metaConnect(message) {
-            if (message.successful) {
-                $('#monocle-status').html("Connected");
-                $('#monocle-status').css("border", "1px solid black");
-                $('#monocle-status').css("color", "black");
-            } else {
-                $('#monocle-status').html("Disconnected");
-                $('#monocle-status').css("border", "1px solid red");
-                $('#monocle-status').css("color", "red");
-            }
-
-            if (_disconnecting) {
-                _connected = false;
-                _connectionClosed();
-            }
-            else {
-                _wasConnected = _connected;
-                _connected = message.successful === true;
-                if (!_wasConnected && _connected) {
-                    _connectionEstablished();
-                }
-                else if (_wasConnected && !_connected) {
-                    _connectionBroken();
-                }
-            }
-        }
-
-        function _metaHandshake(message) {
-            if (message.successful) {
-                _connectionInitialized();
-            }
-        }
-
-        $.cometd.addListener('/meta/handshake', _metaHandshake);
-        $.cometd.addListener('/meta/connect', _metaConnect);
-
-        $(window).unload(function() {
-            if ($.cometd.reload) {
-                $.cometd.reload();
-            }
-            else {
-                $.cometd.disconnect();
-            }
-        });
     }
 })(jQuery);
